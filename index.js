59ab88ea66511fbd965993e7fb0eb19330276ff4
Aruna Turay <tura_arun@yahoo.com>
2023-05-09 15:47:40 -0400

    iterate over the arra with a forEach method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("js loading...")
 fetch("http://localhost:3000/songs")
   .then(res => res.json())
   .then(songs => {
-   // songs.forEach((song) => {
+    songs.forEach((song) => {
 
       //songListMaker(song)
      // createCardElement(song)
