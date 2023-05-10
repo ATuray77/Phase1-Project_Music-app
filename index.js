475d2380375a4be67ef70f8adcd5d47f29935c34
Aruna Turay <tura_arun@yahoo.com>
2023-05-09 20:43:29 -0400

    making a fetch for a POST

--- index.js
+++ index.js
@@ -1,77 +1,75 @@
-console.log("js ..loading")
+console.log("js ..loading");
 
 fetch("http://localhost:3000/songs")
-  .then(res => res.json())
-  .then(songs => {
+  .then((res) => res.json())
+  .then((songs) => {
     songs.forEach((song) => {
-
       //songListMaker(song)
-     createCardElement(song)
-    })
-  })
+      createCardElement(song);
+    });
+  });
 
-  const createCardElement = (song) => {
-    let card = document.createElement("div")
-    card.classList.add("card")
-    
-    let h2 = document.createElement("h2")
-    h2.textContent = song.name
+const createCardElement = (song) => {
+  let card = document.createElement("div");
+  card.classList.add("card");
 
-    let h3 = document.createElement("h3")
-    h3.textContent = song.artist
+  let h2 = document.createElement("h2");
+  h2.textContent = song.name;
 
-    let img = document.createElement("img")
-    img.src = song.image
-    img.classList.add("tubeLink")
-  
-    let pLikes = document.createElement("p")
-    pLikes.textContent = `${song.likes} likes`
+  let h3 = document.createElement("h3");
+  h3.textContent = song.artist;
 
-    let cardButton = document.createElement("button")
+  let img = document.createElement("img");
+  img.src = song.image;
+  img.classList.add("tubeLink");
 
-    //will come back to this when adding event listener to update likes
-    cardButton.addEventListener('click', () => {
-      pLikes.textContent = `${song.likes += 1} likes`
-      updateLikes(song.id, song.likes)
+  let pLikes = document.createElement("p");
+  pLikes.textContent = `${song.likes} likes`;
 
-     })
+  let cardButton = document.createElement("button");
 
-    cardButton.classList.add("like-btn");
-    cardButton.id = song.id;
-    cardButton.textContent = "Like ❤️";
+  //will come back to this when adding event listener to update likes
+  cardButton.addEventListener("click", () => {
+    pLikes.textContent = `${(song.likes += 1)} likes`;
+    updateLikes(song.id, song.likes);
+  });
 
-    card.append(h2, h3, img, pLikes, cardButton)
-    document.getElementById("song-collection").appendChild(card)
-    //document.getElementById("card_container").appendChild("song-collection")
+  cardButton.classList.add("like-btn");
+  cardButton.id = song.id;
+  cardButton.textContent = "Like ❤️";
 
-  }
-  
-  //function to updates likes (PATCH)
-  function updateLikes(id, newNumberOfLikes) {
-    fetch(`http://localhost:3000/songs/${id}`, { 
+  card.append(h2, h3, img, pLikes, cardButton);
+  document.getElementById("song-collection").appendChild(card);
+  //document.getElementById("card_container").appendChild("song-collection")
+};
+
+//function to updates likes (PATCH)
+function updateLikes(id, newNumberOfLikes) {
+  fetch(`http://localhost:3000/songs/${id}`, {
     method: "PATCH",
-    headers:
-    {
+    headers: {
       "Content-Type": "application/json",
-      Accept: "application/json"
+      Accept: "application/json",
     },
-    
+
     body: JSON.stringify({
-      "likes": newNumberOfLikes
-    })
-      })
-    }
+      likes: newNumberOfLikes,
+    }),
+  });
+}
 
-    //Creates a playlist of songs to play in the player
-    const form = document.querySelector("#song-form")
-    const playList = document.querySelector("playlist")
-    const player = document.querySelector("#player")
+//TO ADD A NEW SONG: first adds an event listener to form
+const form = document.querySelector("song-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = Objects.fromEntries(new formData(event.target))
 
-    //adds eventlistener to the form
-    form.addEventListener('submit', function (e) {
-      e.preventDefault()
-      const formData = Object.fromEntries(new formData(e.target))
-      
-      displayPlaylist(song)
-      form.reset()
-    })
+  sendItOut(formData);
+});
+
+//function to make a POST
+function sendItOut(newSong) {
+  fetch("http://localhost:3000/songs"), {
+    
+  }
+}
